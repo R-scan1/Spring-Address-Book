17f519910582dccd87d4cbd065104ee48679557e
R-scan1 <deepr7590@gmail.com>
2025-03-01 20:43:50 +0530

    [Raj] add. introducing dto and model

--- SpringAddressBook/src/main/java/com/bridgelabz/springaddressbook/controller/AddressBookController.java
+++ SpringAddressBook/src/main/java/com/bridgelabz/springaddressbook/controller/AddressBookController.java
@@ -1,37 +1,47 @@
 package com.bridgelabz.springaddressbook.controller;
 
+import com.bridgelabz.springaddressbook.dto.AddressBookDTO;
+import com.bridgelabz.springaddressbook.dto.ResponseDTO;
+import com.bridgelabz.springaddressbook.model.AddressBook;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/addressbook")
+@RequestMapping("/addressbook")
 public class AddressBookController {
 
-    @GetMapping("/contacts")
-    public ResponseEntity<String> getAllContacts() {
-        return ResponseEntity.ok("{\"message\": \"Fetching all contacts...\"}");
+    @GetMapping("/")
+    public ResponseEntity<ResponseDTO> getAllAddresses() {
+        AddressBook address = new AddressBook(1, "John Doe", "1234567890", "New York");
+        ResponseDTO respDTO = new ResponseDTO("Get Call Successful", address);
+        return new ResponseEntity<>(respDTO, HttpStatus.OK);
     }
 
-    @GetMapping("/contacts/{id}")
-    public ResponseEntity<String> getContactById(@PathVariable int id) {
-        return ResponseEntity.ok("{\"message\": \"Fetching contact with ID: " + id + "\"}");
+    @GetMapping("/get/{id}")
+    public ResponseEntity<ResponseDTO> getAddressById(@PathVariable int id) {
+        AddressBook address = new AddressBook(id, "John Doe", "1234567890", "New York");
+        ResponseDTO respDTO = new ResponseDTO("Get Call For ID Successful", address);
+        return new ResponseEntity<>(respDTO, HttpStatus.OK);
     }
 
-    @PostMapping("/contacts")
-    public ResponseEntity<String> addContact(@RequestBody String contact) {
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body("{\"message\": \"Adding new contact: " + contact + "\"}");
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDTO> createAddress(@RequestBody AddressBookDTO addressBookDTO) {
+        AddressBook address = new AddressBook(1, addressBookDTO.getName(), addressBookDTO.getPhoneNumber(), addressBookDTO.getCity());
+        ResponseDTO respDTO = new ResponseDTO("Created Address Successfully", address);
+        return new ResponseEntity<>(respDTO, HttpStatus.OK);
     }
 
-    @PutMapping("/contacts/{id}")
-    public ResponseEntity<String> updateContact(@PathVariable int id, @RequestBody String contact) {
-        return ResponseEntity.ok("{\"message\": \"Updating contact with ID: " + id + " to: " + contact + "\"}");
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDTO> updateAddress(@RequestBody AddressBookDTO addressBookDTO) {
+        AddressBook address = new AddressBook(1, addressBookDTO.getName(), addressBookDTO.getPhoneNumber(), addressBookDTO.getCity());
+        ResponseDTO respDTO = new ResponseDTO("Updated Address Successfully", address);
+        return new ResponseEntity<>(respDTO, HttpStatus.OK);
     }
 
-    @DeleteMapping("/contacts/{id}")
-    public ResponseEntity<String> deleteContact(@PathVariable int id) {
-        return ResponseEntity.status(HttpStatus.NO_CONTENT)
-                .body("{\"message\": \"Deleting contact with ID: " + id + "\"}");
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ResponseDTO> deleteAddress(@PathVariable int id) {
+        ResponseDTO respDTO = new ResponseDTO("Deleted Successfully", "Deleted ID: " + id);
+        return new ResponseEntity<>(respDTO, HttpStatus.OK);
     }
 }
---
+++ SpringAddressBook/src/main/java/com/bridgelabz/springaddressbook/dto/AddressBookDTO.java
@@ -0,0 +1,41 @@
+package com.bridgelabz.springaddressbook.dto;
+
+public class AddressBookDTO {
+
+    private String name;
+    private String phoneNumber;
+    private String city;
+
+    public AddressBookDTO() {
+    }
+
+    public AddressBookDTO(String name, String phoneNumber, String city) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
